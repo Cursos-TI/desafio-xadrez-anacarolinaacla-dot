cc61b445ab281688daaf43e89b39be02048ec518
anacarolinaacla-dot <anacarolina.acla@gmail.com>
2026-02-25 01:37:24 +0000

    desafio pronto

--- xadrez.c
+++ xadrez.c
@@ -7,26 +7,47 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int movimentosTorre = 5;
+    int movimentosBispo = 5;
+    int movimentosRainha = 8;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("--- Movimento do Bispo --- \n");
 
+    int i = 0;
+
+    while (i < movimentosBispo)
+    {
+        printf("Direita, Cima\n", i);
+        i++;
+    }
+
+    printf("\n");
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Sugestão: Utilize uma  Comentário: Utilizei o forestrutura de repetição para simular a movimentação da Torre para a direita.
+    
+    printf("--- Movimento da Torre ---\n");
 
+    for (int i = 0; i < movimentosTorre; i++)
+    {
+        printf("Direita\n", i);
+    }
+
+    printf("\n");
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    printf("--- Movimento da Rainha---\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    int contadorRainha = 0;
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    }
+    while (contadorRainha < movimentosRainha);
+
 
     return 0;
 }
